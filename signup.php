558278cc2b049b6a9212063877b06ea8d96f0496
Dorienn <frostvind43@gmail.com>
2023-04-30 17:56:40 +0800

    Create signup.php

---
+++ signup.php
@@ -0,0 +1,206 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <title>Barangay 872, Pandacan | Official Website </title>
+        <!-- BOOTSTRAP -->
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+        <!-- CALENDAR -->
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.9.0/css/bootstrap-datepicker.min.css">
+		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">		
+        <!-- FA ICONS -->
+        <link rel="stylesheet" href="path/to/font-awesome/css/font-awesome.min.css">
+    
+        <style>
+            .divider {
+                height: 3rem;
+                background-color: rgba(0, 0, 0, .1);
+                border: solid rgba(0, 0, 0, .15);
+                border-width: 1px 0;
+                box-shadow: inset 0 .5em 1.5em rgba(0, 0, 0, .1), inset 0 .125em .5em rgba(0, 0, 0, .15);
+            }
+            .form-login {
+                max-width: 350px;
+                padding: 15px;
+                padding-top: 150px;
+                padding-bottom: 150px;
+                margin: auto;
+                width: 100%;
+            }
+        </style>
+
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.1.3/js/bootstrap.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.9.0/js/bootstrap-datepicker.min.js"></script>
+        <script>
+            $(function(){
+                $('#datepicker').datepicker();
+            });
+        </script>
+    </head>
+    <body>
+        <!-- BOOTSTRAP JS -->
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>        
+        
+        <!-- NAVBAR START -->
+        <div class="container py-3 px-0">
+            <header class="d-flex flex-wrap align-items-center justify-content-center justify-content-md-between py-3">
+                <a href="home.php" class="d-flex align-items-center col-md-3 mb-2 mb-md-0 text-dark text-decoration-none">
+                    <img src="assets/img/logo.png" height="80px" width="auto">
+                </a>
+
+                <ul class="nav col-12 col-md-auto mb-2 justify-content-center mb-md-0">
+                    <li><a href="home.php" class="nav-link px-2 link-secondary">Home</a></li>
+                    <li><a href="about.php" class="nav-link px-2 link-dark">About Us</a></li>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle link-dark" href="#" id="dropdown" data-bs-toggle="dropdown" aria-expanded="false">Services</a>
+                        <ul class="dropdown-menu" aria-labelledby="dropdown">
+                            <li><a class="dropdown-item" href="#">Request Document</a></li>
+                            <li><a class="dropdown-item" href="#">View Appointments</a></li>
+                        </ul>
+                    </li>
+                    <li class="nav-item"><a class="nav-link px-2 link-dark " href="contact.php">Contact Us</a></li>
+                    <li class="nav-item"><a class="nav-link px-2 link-dark " href="faq.php">F.A.Q.</a></li>
+                    <li class="nav-item"><a class="nav-link px-2 link-dark " href="login.php">Login</a></li>
+                    <li class="nav-item"><a class="nav-link px-2 link-dark " href="signup.php">Sign-up</a></li>
+                </ul>
+                
+            </header>
+        </div>
+        <!-- NAVBAR END -->
+
+        <!-- <div class="divider"></div> -->
+
+        <section class="border-top">
+
+        <div class="container py-5">
+            <h2 class="mt-3 mb-5 text-center">Sign-up</h2>
+            <form class="needs-validation" novalidate="">
+            <div class="row g-3">
+                <div class="col-sm-6">
+                <label for="firstName" class="form-label">First name</label>
+                <input type="text" class="form-control" id="firstName" placeholder="Jose" value="" required="">
+                <div class="invalid-feedback">
+                    Valid first name is required.
+                </div>
+                </div>
+
+                <div class="col-sm-6">
+                <label for="lastName" class="form-label">Last name</label>
+                <input type="text" class="form-control" id="lastName" placeholder="Cruz" value="" required="">
+                <div class="invalid-feedback">
+                    Valid last name is required.
+                </div>
+                </div>
+
+                <div class="col-sm-6">
+                <label for="contactNo" class="form-label">Contact No.</label>
+                <input type="contactNo" class="form-control" id="contactNo" placeholder="09123456789">
+                <div class="invalid-feedback">
+                    Please enter a valid contact number.
+                </div>
+                </div>
+
+                <div class="col-sm-6">
+                <label for="email" class="form-label">Email <span class="text-muted">(Optional)</span></label>
+                <input type="email" class="form-control" id="email" placeholder="you@example.com">
+                <div class="invalid-feedback">
+                    Please enter a valid email address.
+                </div>
+                </div>
+
+                
+                <div class="col-12">
+                <label for="password" class="form-label">Password</label>
+                <input type="password" class="form-control" id="password" placeholder="Password" required="">
+                <div class="invalid-feedback">
+                    Please enter your password.
+                </div>
+                </div>
+
+                <div class="col-12">
+                <label for="address" class="form-label">Address</label>
+                <input type="text" class="form-control" id="address" placeholder="1234 Main St" required="">
+                <div class="invalid-feedback">
+                    Please enter your address.
+                </div>
+                </div>
+
+                <div class="col-sm-6">
+                    <label for="dateOfBirth" class="form-label">Date</label>
+                        <div class="input-group date" id="datepicker">
+                            <input type="text" class="form-control" id="dateOfBirth"/>
+                            <span class="input-group-append">
+                                <span class="input-group-text bg-light d-block">
+                                    <i class="fa fa-calendar" aria-hidden="true"></i>
+                                </span>
+                            </span>
+                        </div>
+                </div>
+
+                <div class="col-sm-6">
+                    <label for="dateOfBirth" class="form-label">Marital Status</label>
+                    <select class="form-select">
+                        <option selected value="Single">Single</option>
+                        <option value="Married">Married</option>
+                        <option value="Widowed">Widowed</option>
+                        <option value="Divorced">Divorced</option>
+                    </select>
+                </div>
+
+                <div class="col-12">
+                    <label for="isDeceased" class form-label>Registering for the deceased?</label>
+                    <input class="form-check-input" type="checkbox" value="" id="isDeceased">
+                </div>
+
+
+            </div>
+            <div class="text-center mt-5">
+                <button class="btn btn-primary btn-lg" type="submit">Confirm</button>
+            </div>
+            </form>
+        </div>
+
+        </section>
+
+        <!-- FOOTER START -->
+        <div class="container-fluid pb-2 px-0 border-top bg-light">
+            <div class="container">
+                <footer class="footer mt-auto py-3">
+                    <div class="container-fluid py-5">
+                        <div class="row px-5">
+                            <div class="mx-auto col-md-3 px-3">
+                                <!-- EVEN SHORTER SUMMARY OF ABOUT US -->
+                                <h3 class="text-center">About Us</h3>
+                                <span class="text-muted text-center">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum sit amet est nibh. Nam vel erat et nisi viverra porttitor malesuada consectetur nibh.</span>
+                            </div>
+                            <div class="mx-auto col-md-3 px-3">
+                                <h3 class="text-center">Links</h3>
+                                <ul class="nav col-12 col-md-auto mb-2 justify-content-center mb-md-0 list-group">
+                                    <li><a href="home.php" class="nav-link px-2 link-dark text-center">Home</a></li>
+                                    <li><a href="about.php" class="nav-link px-2 link-dark text-center">About Us</a></li>
+                                    <li><a href="#" class="nav-link px-2 link-dark text-center">Request Document</a></li>
+                                    <li><a href="#" class="nav-link px-2 link-dark text-center">View Appointments</a></li>
+                                    <li><a href="faq.php" class="nav-link px-2 link-dark text-center">F.A.Q.</a></li>
+                                </ul>
+                            </div>
+                            <div class="mx-auto col-md-3 px-3">
+                                <h3 class="text-center">Contact Us</h3>
+                                <span class="text-muted text-center">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum sit amet est nibh. Nam vel erat et nisi viverra porttitor malesuada consectetur nibh.</span>
+                            </div>
+                            <div class="mx-auto col-md-3 px-3">
+                                <h3 class="text-center">Location</h3>
+                                <div class="embed-responsive text-center">
+                                    <iframe class="embed-responsive-item" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3861.28219962877!2d121.00253820000002!3d14.582989050000002!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3397c993b7ce58bf%3A0x32d5ccfe4b774f6d!2sBrgy.%20872%2C%20Pandacan%2C%20Manila%2C%201011%20Metro%20Manila!5e0!3m2!1sen!2sph!4v1682255049225!5m2!1sen!2sph" style="border:0; min-height:300px;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </footer>
+            </div>
+        </div>
+        <!-- FOOTER END -->
+    </body>
+</html>
